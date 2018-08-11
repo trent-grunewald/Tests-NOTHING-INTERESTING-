122a73ea36894c2f0a1669c5f68f446af839dbca
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-08-10 23:15:23 -0700

    test

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -1,41 +1,73 @@
 //BY ID
-console.log(document.getElementById('task-title'));
+// console.log(document.getElementById('task-title'));
 
-const taskTitle = document.getElementById('task-title');
+// const taskTitle = document.getElementById('task-title');
 
-//-----------------------------------------------------------------CHANGE STYLE
-
-
-//usually used during an event / dynamic functionality
-taskTitle.style.background = "green";
-taskTitle.style.color = "white";
-taskTitle.style.padding = "5px";
-
-//----------------------------------------------------------------CHANGE CONTENT
+// //-----------------------------------------------------------------CHANGE STYLE
 
 
-taskTitle.textContent = "task ListI";
-taskTitle.innerText = "MY TASKS";
-taskTitle.innerHTML = "<span syle='color:red'>TRENTS LIST</span>";
+// //usually used during an event / dynamic functionality
+// taskTitle.style.background = "green";
+// taskTitle.style.color = "white";
+// taskTitle.style.padding = "5px";
+
+// //----------------------------------------------------------------CHANGE CONTENT
 
 
-// -------------------------------------------------------------QUERY SELECTOR 
+// taskTitle.textContent = "task ListI";
+// taskTitle.innerText = "MY TASKS";
+// taskTitle.innerHTML = "<span syle='color:red'>TRENTS LIST</span>";
 
 
-//more powerful, can use any CSS selector
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
+// // -------------------------------------------------------------QUERY SELECTOR 
 
-document.querySelector('li').style.color = 'red';
-//more specific
-document.querySelector('ul li').style.color = 'red';
-//Specific LI
-document.querySelector('li:last-child').style.fontWeight = 'bold';
-//SPECIFIC LI
-document.querySelector('li:nth-Child(3)').style.color = 'orange';
-document.querySelector('li:nth-Child(3)').textContent = 'HEYOOOooooo';
+
+// //more powerful, can use any CSS selector
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
+
+// document.querySelector('li').style.color = 'red';
+// //more specific
+// document.querySelector('ul li').style.color = 'red';
+// //Specific LI
+// document.querySelector('li:last-child').style.fontWeight = 'bold';
+// //SPECIFIC LI
+// document.querySelector('li:nth-Child(3)').style.color = 'orange';
+// document.querySelector('li:nth-Child(3)').textContent = 'HEYOOOooooo';
 
 // -----------------------------------------------------------MULTIPLE ELEMENTS
 
 
+// const items = document.getElementsByClassName('collection-item');
+
+// console.log(items);
+// console.log(items[0]);
+
+// //ACCESSING SPLECIFIC ITEM IN CLASS
+// items[0].style.color = 'blue';
+// items[3].textContent = 'I am test!';
+
+
+// //SPECIFIC ELEMENT BY CLASS
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+
+
+//BY TAG NAME
+let lis = document.getElementsByTagName('li');
+// console.log(lis);
+
+
+//CONVERT HTML COLLECTION INTO AN ARRAY
+lis = Array.from(lis);
+
+//Reverse Array
+lis.reverse();
+
+//FOR EACH - This logs all class names, changes text, and gives index positions(Reversed since the above)
+lis.forEach(function(li, index){
+  console.log(li.className);
+  li.textContent = `${index}: Hello`;
+});
+
+console.log(lis);
