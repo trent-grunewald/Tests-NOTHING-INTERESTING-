8b50da10707c00d17e79a4f99c613e8c6df2fd5e
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-08-10 22:08:22 -0700

    tests

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -1,2 +1,30 @@
 //BY ID
-console.log(document.getElementById('task-title'))
+console.log(document.getElementById('task-title'));
+
+const taskTitle = document.getElementById('task-title');
+
+//CHANGE STYLE
+//usually used during an event / dynamic functionality
+taskTitle.style.background = "green";
+taskTitle.style.color = "white";
+taskTitle.style.padding = "5px";
+
+//CHANGE CONTENT
+taskTitle.textContent = "task ListI";
+taskTitle.innerText = "MY TASKS";
+taskTitle.innerHTML = "<span syle='color:red'>TRENTS LIST</span>";
+
+
+//QUERY SELECTOR 
+//more powerful, can use any CSS selector
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red';
+//more specific
+document.querySelector('ul li').style.color = 'red';
+//Specific LI
+document.querySelector('li:last-child').style.fontWeight = 'bold';
+//SPECIFIC LI
+document.querySelector('li:nth-Child(3)').style.color = 'orange';
