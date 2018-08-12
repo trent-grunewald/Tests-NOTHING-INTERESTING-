40eb992bc8b9f0b2c9db2a98665d090469840a12
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-08-11 22:18:47 -0700

    tests

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -337,3 +337,36 @@ const heading = document.querySelector('h5');
 //   // taskInput.value = '';
 //   // e.preventDefault();
 // }
+
+
+// ------------------------------------------------------EVENT BUBBLING-----
+
+
+// document.querySelector('.card-title').addEventListener('click', function(){
+//   console.log('card-title');
+// });
+
+// document.querySelector('.card-content').addEventListener('click', function(){
+//   console.log('card-content');
+// });
+
+// document.querySelector('.card').addEventListener('click', function(){
+//   console.log('card');
+// });
+
+// document.querySelector('.col').addEventListener('click', function(){
+//   console.log('col');
+// });
+
+
+//------------------------------------------------EVENT DELEGATION------------
+
+
+document.body.addEventListener('click', deleteItem);
+
+function deleteItem(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    console.log('Deleted');
+    e.target.parentElement.parentElement.remove();
+    }
+  }
