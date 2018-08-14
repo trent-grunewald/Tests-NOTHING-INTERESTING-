d1cade3e77ac35186700bf72c88c697461e714cf
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-08-14 10:41:42 -0700

    Loan calculator

---
+++ loanCalculator/app.js
@@ -0,0 +1,8 @@
+//Listen for submit
+document.querySelector('loan-form').addEventListener('submit',calculateResults);
+
+//calculate Results
+function calculateResults(e){
+
+  e.preventDefault();
+}
