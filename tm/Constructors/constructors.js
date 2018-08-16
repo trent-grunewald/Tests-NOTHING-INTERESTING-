351109e1ed19e826d01c84c592d67b8420a6aa22
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-08-15 19:29:51 -0700

    Don't use NEW

--- tm/Constructors/constructors.js
+++ tm/Constructors/constructors.js
@@ -1,18 +1,31 @@
-//Object Literal
-const person = {
-  name: 'Trent',
-  age: 31,
+// //Object Literal
+// const person = {
+//   name: 'Trent',
+//   age: 31,
+// }
+
+// //Constructor -Start capitalized
+// //date has to be capitalized
+// function Alien(name, age, birthday) {
+//   this.name = name;
+//   this.birthday = new Date(birthday);
+//   this.age = birthday;
+// }
+
+// const trent = new Alien('Trent', 30, '02/01/1987');
+// const rachel = new Alien('Rachel', 33, '11/23/1985');
+
+// console.log(trent, rachel);
+
+//String Constructor (DO NOT USE)
+
+const name1 = 'jeff';
+const name2 = new String('jeff');
+
+console.log(typeof name2);
+
+if(name1 === 'jeff'){
+  console.log('yes');
+}else{
+  console.log('no');
 }
-
-//Constructor -Start capitalized
-//date has to be capitalized
-function Alien(name, age, birthday) {
-  this.name = name;
-  this.birthday = new Date(birthday);
-  this.age = birthday;
-}
-
-const trent = new Alien('Trent', 30, '02/01/1987');
-const rachel = new Alien('Rachel', 33, '11/23/1985');
-
-console.log(trent, rachel);
