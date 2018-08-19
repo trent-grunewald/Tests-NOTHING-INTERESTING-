638133f3fcaf38f4ce6ce5b75929d633e4b726f5
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-08-19 10:17:02 -0700

    JSON

--- tm/Booklist/ajaxFetchAPI/AsynchAjaxFetchAPI.js
+++ tm/Booklist/ajaxFetchAPI/AsynchAjaxFetchAPI.js
@@ -46,9 +46,11 @@
 
 
 // ----------------------------------------------------------JSON----------------
-
+//-----------------------------------------JAVASCRIPT OBJECT NOTATION
 
 document.getElementById('button1').addEventListener('click', loadCustomer);
+
+document.getElementById('button2').addEventListener('click', loadCustomers);
 
 function loadCustomer(e) {
   const xhr = new XMLHttpRequest();
@@ -57,9 +59,47 @@ function loadCustomer(e) {
 
   xhr.onload = function() {
     if(this.status === 200) {
-      console.log(this.responseText);
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+      `;
+
+      document.getElementById('customer').innerHTML = output;
     }
   }
 
   xhr.send();
 }
+
+//LOAD CUSTOMERS 
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const customers = JSON.parse(this.responseText);
+        let output = '';  
+          customers.forEach(function(customers){
+            output += 
+              output = `
+                <ul>
+                  <li>ID: ${customers.id}</li>
+                  <li>Name: ${customers.name}</li>
+                  <li>Company: ${customers.company}</li>
+                  <li>Phone: ${customers.phone}</li>
+                </ul>
+                `;
+             })
+          document.getElementById('customers').innerHTML = output;
+        }
+      }
+    xhr.send();
+  }
