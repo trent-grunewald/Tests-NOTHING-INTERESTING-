b1178b23134c8e60b2f761fc201d2359855d5fed
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-08-19 08:48:23 -0700

    JSON

--- tm/Booklist/ajaxFetchAPI/AsynchAjaxFetchAPI.js
+++ tm/Booklist/ajaxFetchAPI/AsynchAjaxFetchAPI.js
@@ -48,3 +48,18 @@
 // ----------------------------------------------------------JSON----------------
 
 
+document.getElementById('button1').addEventListener('click', loadCustomer);
+
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customer.json', true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      console.log(this.responseText);
+    }
+  }
+
+  xhr.send();
+}
