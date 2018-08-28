308fc19356189ff3a1b0afa7747cb30dc2575e64
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-08-27 17:47:28 -0700

    FINAL
    Added repos

--- tm/githubSearch/github.js
+++ tm/githubSearch/github.js
@@ -2,13 +2,19 @@ class GitHub {
   constructor() {
     this.client_id = '45e05420d198509808aa';
     this.client_secret = '326f2442493862155015285ee838288b99cbd05c';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}??client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
     return {
-      profile 
+      profile, 
+      repos
     }
   }
 }
